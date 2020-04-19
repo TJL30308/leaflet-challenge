b6a0a0d66a7ea98b7c7643be9fdac41a9eeb3665
Topias Lemetyinen <topias.lemetyinen@gmail.com>
2020-04-19 10:10:14 -0400

    updates

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -4,7 +4,7 @@ var myMap = L.map("map", {
     zoom: 2
   });
   
-  // Adding tile layer
+
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
     maxZoom: 13,
@@ -29,20 +29,25 @@ function createMarkers(data){
     var mag = data.features[i].properties.mag;
     var place = data.features[i].properties.place;
 
-    // console.log(mag);
+    function getColor(d) {
+      return d >= 5 ? '#880000' : 
+        d >= 4 ? '#FF2B2B' : 
+        d >= 3 ? '#FF5252' :
+        d >= 2 ? '#FD8E8E' :
+        d>= 1 ? '#FFE0E0' : 
+        '#FFFFFF';
+    }
 
-    // console.log(lat, lng);
-
-    L.circle([lat, lng], {
+    L.circle([lat, lng], { 
+      stroke: false,
+      color: getColor(mag),
+      fill: getColor(mag),
       fillOpacity: 0.75,
-      color: "red",
-      fillColor: "red",
       radius: mag * 30000
     }).bindPopup("<h4> Location: " + place + "</h4>" + "<p> Magnitude: " + mag + "</p>").addTo(myMap);
-  
+
   };
 
 };
 
-// API Call to request earthquake data
 d3.json(earthquakeURL, createMarkers);
